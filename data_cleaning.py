3315880b30c00831d60e1f39b9088f7df8120378
mdrhmn48 <mdrhmn48@gmail.com>
2023-05-24 08:17:28 -0400

    added comment on each section

--- data_cleaning.py
+++ data_cleaning.py
@@ -1,27 +1,42 @@
 from pyspark.sql import SparkSession
+
+# Create SparkSession
 spark = SparkSession.builder.master("local[*]").getOrCreate()
+
+# Configure SparkSession
 spark = SparkSession.builder\
         .master("local")\
         .appName("Colab")\
         .config('spark.ui.port', '4050')\
         .getOrCreate()
+
+# Print SparkSession
 spark
 
 import pyspark.sql.functions as F
-
 import os
 import logging
 import boto3
 from botocore.exceptions import ClientError
 from KEYS import ACCESS_KEY, SECRET_ACCESS_KEY
 
+# Create S3 client
 s3_client = boto3.client(
     's3',
     aws_access_key_id=ACCESS_KEY,
     aws_secret_access_key=SECRET_ACCESS_KEY
-    )
+)
 
 def upload_file(file_name, bucket, object_name=None):
+    """
+    Function to upload a file to S3 bucket.
+    Args:
+        file_name (str): Local file path.
+        bucket (str): S3 bucket name.
+        object_name (str, optional): Object name in S3. If not provided, the file name will be used.
+    Returns:
+        bool: True if the file upload is successful, False otherwise.
+    """
     if object_name is None:
         object_name = os.path.basename(file_name)
 
@@ -35,6 +50,15 @@ def upload_file(file_name, bucket, object_name=None):
     return True
 
 def download_file(file_name, bucket, object_name):
+    """
+    Function to download a file from S3 bucket.
+    Args:
+        file_name (str): Local file path.
+        bucket (str): S3 bucket name.
+        object_name (str): Object name in S3.
+    Returns:
+        bool: True if the file download is successful, False otherwise.
+    """
     try:
         res = s3_client.download_file(bucket, object_name, file_name)
         if res:
@@ -47,41 +71,31 @@ def download_file(file_name, bucket, object_name):
 bucket = "capstone-project-team2"
 file1 = "LOL_items_stats.csv"
 file2 = "lol_champion_stats_en.csv"
+
+# Download files from S3 bucket
 download_file(file1, bucket=bucket, object_name=file1)
 download_file(file2, bucket=bucket, object_name=file2)
 
-df1 = spark.read.csv(file1,encoding='ISO-8859-1', header=True, sep =';')
-df2 = spark.read.csv(file2,encoding='ISO-8859-1', header=True, sep =';')
+# Read CSV files into DataFrames
+df1 = spark.read.csv(file1, encoding='ISO-8859-1', header=True, sep=';')
+df2 = spark.read.csv(file2, encoding='ISO-8859-1', header=True, sep=';')
 
-#changing the data types to float
+# Changing the data types to float
 from pyspark.sql.functions import *
 cols = ["Cost","Sell", 'AS', 'Crit', 'LS', 'APen', 'AP','AH', 'Mana','MP5','HSP','OVamp', 'MPen', 'Health', 'Armor',"MR","HP5","MS"]
 for col_name in cols:
     df1 = df1.withColumn(col_name, col(col_name).cast('float'))
 
-#Making Item column primary key(nullable=false)
+# Making Item column primary key (nullable=false)
 df1 = df1.withColumn("Item", F.coalesce(F.col("Item"), F.lit(0)))
 
-#replacing null value to unknown for Maps Column
+# Replacing null values with "Unknown" for Maps column
 df1 = df1.na.fill(value="Unknown", subset=["Maps"])
 
-# Displaying table
+# Displaying table schema and data
 df1.printSchema()
 df1.show(200)
 
 
 # Converting champion_name to non-nullable
-df2 = df2.withColumn("champion_name", F.coalesce(F.col("champion_name"), F.lit(0)))
-
-#Changing date release to date datatype
-df2 = df2.withColumn('date_release', F.to_timestamp('date_release', 'dd/MM/yyyy'))
-df2 = df2.withColumn('date_release', F.to_date(col("date_release"),"dd-MM-yyyy"))
-
-# Converting columns to double
-cols = cols = [ 'HP', 'HP+', 'HP_lvl18', 'HP5', 'HP5+', 'HP5_lvl18', 'MP', 'MP+', 'MP_lvl18', 'MP5', 'MP5+', 'MP5_lvl18', 'AD', 'AD+', 'AD_lvl18', 'AS', 'AS_lvl18', 'AR', 'AR+', 'AR_lvl18', 'MR', 'MR+', 'MR_lvl18', 'MS', 'MS_lvl18', 'range', 'range_lvl18']
-for col_name in cols:
-    df2 = df2.withColumn(col_name, col(col_name).cast('float'))
-
-# Displaying table
-df2.printSchema()
-df2.show()
+df2 = df2.withColumn("champion_name", F)
